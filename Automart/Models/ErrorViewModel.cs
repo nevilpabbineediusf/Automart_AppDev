441497b697850ecd3eff1ea1cf9d75c6eb0e78f6
Manikanta1210 <manikantareddyr@usf.edu>
2025-05-06 00:44:40 -0400

    Updated the search logic of the search bar

--- Automart/Models/ErrorViewModel.cs
+++ Automart/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Automart.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
